17c111c8d2127af9e0b1893b0f7ec1152c336a23
Afras Rahim <afrasrahim11@gamil.com>
2024-12-11 21:07:49 +0530

    frontend login logout signup done

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
 import { Link } from 'react-router-dom'
+import {UserDataContext} from '../context/userContext'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const UserLogin = () => {
   const [email, setEmail] = useState('')
   const [pasword, setPasword] = useState('')
   const [userData, setUserData] = useState({})
 
-  const submitHandler = (e) => {
+     const {user,setUser}=useContext(UserDataContext)
+     const navigate=useNavigate()
+
+  const submitHandler = async(e) => {
     e.preventDefault();
-    setUserData({
-      email: email,
-      password: pasword
-    })
+    const userData={
+      email:email,
+      password:pasword
+    }
+    const response=await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`,userData)
+    if(response.status===200){
+      const data=response.data
+      
+      setUser(data.user)
+      localStorage.setItem('token',data.token)
+      navigate('/home')
+    }
+
     setEmail('')
     setPasword('')
   }
